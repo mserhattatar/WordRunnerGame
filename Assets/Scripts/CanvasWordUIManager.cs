45626dd1836bb34c77135e0cd367eddc2c5229a8
serhattatar <mserhattatar@gmail.com>
2021-08-31 14:21:04 +0300

    added canvaswordUImanager
    for 6,7,8 letter words

---
+++ Assets/Scripts/CanvasWordUIManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasWordUIManager : MonoBehaviour
+{
+    [SerializeField] private List<PanelLetterController> sixLetterInPanelChar;
+    [SerializeField] private List<PanelLetterController> sevenLetterInPanelChar;
+    [SerializeField] private List<PanelLetterController> eightLetterInPanelChar;
+
+
+    public List<PanelLetterController> ShowLetters(int stringLenght)
+    {
+        var panelLetter = stringLenght switch
+        {
+            6 => sixLetterInPanelChar,
+            7 => sevenLetterInPanelChar,
+            _ => eightLetterInPanelChar
+        };
+        ResetLetters(panelLetter);
+        Debug.Log("stringLenght " + stringLenght);
+        return panelLetter;
+    }
+
+    private void ResetLetters(List<PanelLetterController> panelLetter)
+    {
+        foreach (var six in sixLetterInPanelChar)
+        {
+            six.SetGameObjectVisibility(false);
+        }
+
+        foreach (var seven in sevenLetterInPanelChar)
+        {
+            seven.SetGameObjectVisibility(false);
+        }
+
+        foreach (var eight in eightLetterInPanelChar)
+        {
+            eight.SetGameObjectVisibility(false);
+        }
+
+        foreach (var letter in panelLetter)
+        {
+            letter.SetGameObjectVisibility(true);
+        }
+    }
+}
